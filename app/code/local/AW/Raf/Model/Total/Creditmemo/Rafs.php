1dfd89de6438902c6991ccc41e21668793b35c15
root <root@ip-172-31-43-84.ap-south-1.compute.internal>
2020-06-22 21:47:57 +0000

    referal aheadworks

---
+++ app/code/local/AW/Raf/Model/Total/Creditmemo/Rafs.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * aheadWorks Co.
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the EULA
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://ecommerce.aheadworks.com/AW-LICENSE.txt
+ *
+ * =================================================================
+ *                 MAGENTO EDITION USAGE NOTICE
+ * =================================================================
+ * This software is designed to work with Magento community edition and
+ * its use on an edition other than specified is prohibited. aheadWorks does not
+ * provide extension support in case of incorrect edition use.
+ * =================================================================
+ *
+ * @category   AW
+ * @package    AW_Raf
+ * @version    2.2.0
+ * @copyright  Copyright (c) 2010-2012 aheadWorks Co. (http://www.aheadworks.com)
+ * @license    http://ecommerce.aheadworks.com/AW-LICENSE.txt
+ */
+
+class AW_Raf_Model_Total_Creditmemo_Rafs extends Mage_Sales_Model_Order_Creditmemo_Total_Abstract
+{
+    public function collect(Mage_Sales_Model_Order_Creditmemo $creditMemo)
+    {
+        $_result = parent::collect($creditMemo);
+
+        $order = $creditMemo->getOrder();
+
+        $customer = Mage::getModel('customer/customer')->load($order->getCustomerId());
+        if (!$customer->getId()) {
+            return $this;
+        }
+        $baseDiscount = Mage::getModel('awraf/orderref')->getTotalDiscount($order);
+
+        if (!(float)$baseDiscount) {
+            return $this;
+        }
+
+        $currentRowItemDiscountAmount = 0.00;
+        $qtyToRefund = Mage::app()->getRequest()->getParam('creditmemo', null);
+        foreach ($order->getAllItems() as $item) {
+            if(
+                $item->getProductType() == Mage_Catalog_Model_Product_Type::TYPE_BUNDLE
+                && $item->getProduct()->getPriceType() == Mage_Bundle_Model_Product_Price::PRICE_TYPE_DYNAMIC
+            ) {
+                continue;
+            }
+
+            $rowItemPercentOfSubtotal = $item->getRowTotal() * 100 / $order->getSubtotal();
+            $rowItemRafDiscountAmount = $baseDiscount * $rowItemPercentOfSubtotal / 100;
+
+            if (!isset($qtyToRefund['items'][$item->getId()])) {
+                $rowItemQtyToRefund = $item->getQtyInvoiced() - $item->getQtyRefunded();
+            } else {
+                $rowItemQtyToRefund = $qtyToRefund['items'][$item->getId()]['qty'];
+            }
+            $currentRowItemDiscountAmount += $rowItemRafDiscountAmount / $item->getQtyOrdered() * $rowItemQtyToRefund;
+        }
+
+        $amount = 1.00 * $currentRowItemDiscountAmount;
+        $storeDiscount = Mage::helper('awraf')->convertAmountByCurrencyCode(
+            $amount, $order->getBaseCurrencyCode(), $order->getOrderCurrencyCode()
+        );
+
+        if (!$creditMemo->getId()) {
+            $creditMemo->addData(
+                array(
+                    'grand_total'      => $creditMemo->getData('grand_total') - $storeDiscount,
+                    'base_grand_total' => $creditMemo->getData('base_grand_total') - $currentRowItemDiscountAmount,
+                )
+            );
+        }
+
+        return $_result;
+    }
+}
